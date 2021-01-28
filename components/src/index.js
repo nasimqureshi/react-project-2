452d643d6ea9556cd9456463b58502a493e9c85f
nasimqureshi <qureshi.nasim@yahoo.com>
2021-01-28 17:45:16 -0600

    first commit

---
+++ components/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <>
+  <h1>My name is Nasim</h1>
+  <p>This is my components</p>
+  <ol>
+    <li>Nasim</li>
+    <li>Qureshi</li>
+    <li>Qamar</li>
+    <li>Sana</li>
+  </ol>
+  </>,
+ document.getElementById('root')
+);
+
+
